b7431cdfdc2df78999636e98daac4d1c42201f3d
myid-hana <sosqkrgksml@gmail.com>
2024-07-17 15:41:23 +0900

    font http 비활성화

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  GoogleFonts.config.allowRuntimeFetching = true;
+
   runApp(
     const MyApp(),
   );
