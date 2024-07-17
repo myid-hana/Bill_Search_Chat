ec3e7e1250f583d6f8ea6c3ae4b0525f399cd8aa
myid-hana <sosqkrgksml@gmail.com>
2024-07-17 19:12:11 +0900

    debugShowCheckedModeBanner false 설정

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
         ),
         textTheme: GoogleFonts.notoSansKrTextTheme(),
       ),
+      debugShowCheckedModeBanner: false,
       home: const ChatPage(),
     );
   }
