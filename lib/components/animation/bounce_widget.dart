51b7649e5b46631b331b748daa86cf17afa50ebe
myid-hana <sosqkrgksml@gmail.com>
2024-07-24 11:37:42 +0900

    bounce widget 을 component 로 뺌

---
+++ lib/components/animation/bounce_widget.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class BounceWidget extends StatefulWidget {
+  final Widget child;
+  const BounceWidget({super.key, required this.child});
+
+  @override
+  State<BounceWidget> createState() => _BounceWidgetState();
+}
+
+class _BounceWidgetState extends State<BounceWidget>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1000),
+    );
+    _animation = Tween<double>(begin: 0, end: 20)
+        .chain(CurveTween(curve: Curves.bounceOut))
+        .animate(_animationController);
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _animationController,
+      builder: (context, child) {
+        return Transform.translate(
+          offset: Offset(0, _animation.value),
+          child: child,
+        );
+      },
+      child: widget.child,
+    );
+  }
+}
